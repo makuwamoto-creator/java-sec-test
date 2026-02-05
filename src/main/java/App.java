a071c40af58c8f0bdc51f9d98d3949c6ee00ceda
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-05 11:15:53 +0900

    chore: App.java secret scan test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,7 @@ public class App {
         // AWSのアクセスキー形式（これもダミーですが、形式は完璧に模しています）
         String aws_key = "AKIA" + "1234567890ABCDEF"; // 分割して書くと検知を避けることがあるので、つなげて書いてください
         String aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
         
     }
 }
