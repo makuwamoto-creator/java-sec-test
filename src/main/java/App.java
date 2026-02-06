3ca0274c2873a5b6f5aa9b1fca1daa160b64e3b8
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-06 14:32:31 +0900

    fix: APP.java　脆弱性、バグ対応2回目

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ import java.sql.*;
 public class App {
     public void getUser(String userId) throws Exception {
         Connection conn = null;
+        Statement stmt = null;
+        PreparedStatement pstmt = null;
         try{
             String dbPassword = System.getenv("DB_PASSWORD");
             conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "user", dbPassword);
@@ -17,19 +19,19 @@ public class App {
             // 3. AWS Secret Key (40文字のランダムっぽい文字列)
             //String aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 
-            Statement stmt = conn.createStatement();
+            stmt = conn.createStatement();
             // ❌ 脆弱性テスト用：SQLインジェクションが起きる書き方
             // String query = "SELECT * FROM users WHERE id = " + "`" + userId + "'";
             String query = "SELECT * FROM users WHERE id = ?";
-            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt = conn.prepareStatement(query);
             pstmt.setString(1, userId);
             
             ResultSet rs = stmt.executeQuery(query);
         }
         finally{
-            if(conn != null) {
-                conn.close();
-            }
+            if(stmt != null) {stmt.close()}
+            if(pstmt != null) {pstmt.close();}
+            if(conn != null) {conn.close();}
         }
 
     }
