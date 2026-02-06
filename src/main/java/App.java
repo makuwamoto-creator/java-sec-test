319fbea6e202274f334876e011c03605b0e9bf5e
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-06 15:11:30 +0900

    fix: APP.java　脆弱性仕込み

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,7 @@ import java.sql.*;
 public class App {
     public void getUser(String userId) throws Exception {
         String dbPassword = System.getenv("DB_PASSWORD");
-        PreparedStatement pstmt = null;
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "user", dbPassword)) {
-
             try (Statement stmt = conn.createStatement()) {
                 // ❌ 脆弱性テスト用：SQLインジェクションが起きる書き方
                 // String query = "SELECT * FROM users WHERE id = " + "`" + userId + "'";
@@ -13,7 +11,16 @@ public class App {
                 try (PreparedStatement pstmt = conn.prepareStatement(query)) {
                     pstmt.setString(1, userId);
                 }
-
+            // 1. GitHub Personal Access Token (一番検知されやすい)
+            // 文字列を分割せず、そのまま1つのリテラルとして書くのがコツです
+            //String github_token = "ghp_n0tReAlK3yJuStF0rTeStingPuRp0s3sOnLy12345";
+                
+            // 2. AWS Access Key (AKIAで始まり20文字)
+            //String aws_access_key = "AKIAV7XXXXXXXXXXXXXX"; 
+            
+            // 3. AWS Secret Key (40文字のランダムっぽい文字列)
+            //String aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+            
                 ResultSet rs = stmt.executeQuery(query);
             }
         }
