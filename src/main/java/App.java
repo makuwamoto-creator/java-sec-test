825961c16e5e897c63f3b4d7f091da31b323ccdd
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-06 17:49:45 +0900

    fix: APP.java　脆弱性対応

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,16 +11,6 @@ public class App {
                 try (PreparedStatement pstmt = conn.prepareStatement(query)) {
                     pstmt.setString(1, userId);
                 }
-            // 1. GitHub Personal Access Token (一番検知されやすい)
-            // 文字列を分割せず、そのまま1つのリテラルとして書くのがコツです
-            //String github_token = "ghp_n0tReAlK3yJuStF0rTeStingPuRp0s3sOnLy12345";
-                
-            // 2. AWS Access Key (AKIAで始まり20文字)
-            //String aws_access_key = "AKIAV7XXXXXXXXXXXXXX"; 
-            
-            // 3. AWS Secret Key (40文字のランダムっぽい文字列)
-            //String aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-            
                 ResultSet rs = stmt.executeQuery(query);
             }
         }
