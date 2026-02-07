7773532108ebe7c3b6c2d729fa0a5d841da0aeb3
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-07 23:16:43 +0900

    CRLFインジェクション対応6

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -19,7 +19,7 @@ public class App {
         // 🚨 SonarQube が「汚染源（Taint Source）」として認識しやすい入力方法
         String input = scanner.nextLine(); 
 
-        String safeinput = "User logged in: " + input.replaceAll("[\r\n]", "");
+        String safeinput = "User logged in: " + input.replace('\n', '_').replace('\r', '_');
         // 🚨 汚染されたデータをそのままログに流す
         logger.info(safeinput); 
         
