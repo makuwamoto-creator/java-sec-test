c860bc36e8157943ebede39446e5495a16217151
masahiko.kuwamoto <masahiko.kuwamoto@shiftsecurity.jp>
2026-02-09 18:37:05 +0900

    spring boot app command injection対応5

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -16,6 +16,7 @@ import jakarta.validation.constraints.Pattern;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Set;
 import java.util.Optional;
@@ -76,7 +77,7 @@ public class UserController {
 
         String finalSafeNames = new String(safeFileName.get().toCharArray());
 
-        File file = new File(String.format("src/main/resources/static/%s", finalSafeNames));
+        File file = new File(MessageFormat.format("src/main/resources/static/{0}", finalSafeNames));
         return new String(Files.readAllBytes(file.toPath()), java.nio.charset.StandardCharsets.UTF_8);
     
     }
